04b32927a38ef7444fd5069c8343cf2efce7d31d
hikmetuygur <uygurhikmet@gmail.com>
2023-01-31 02:53:39 +0300

    Added readme and gitignore

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# RickNMorty
+# RickNMorty App
+
+Full featured iOS app showcasing the Rick and Morty API.
+
+-Written in Swift
+-Unit Tested
--- RickNMorty/Controllers/Core/RMTabViewController.swift
+++ RickNMorty/Controllers/Core/RMTabViewController.swift
@@ -7,14 +7,53 @@
 
 import UIKit
 
-class RMTabViewController: UITabBarController {
+final class RMTabViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        setUpTabs()
     }
-
+    
+    private func setUpTabs() {
+        let characterVC = RMCharacterViewController()
+        let locationVC = RMLocationViewController()
+        let episodesVC = RMEpisodeViewController()
+        let settingsVC = RMSettingsViewController()
+        
+        characterVC.navigationItem.largeTitleDisplayMode = .automatic
+        locationVC.navigationItem.largeTitleDisplayMode = .automatic
+        episodesVC.navigationItem.largeTitleDisplayMode = .automatic
+        settingsVC.navigationItem.largeTitleDisplayMode = .automatic
+        
+        let navChar = UINavigationController(rootViewController: characterVC)
+        let navLoc = UINavigationController(rootViewController: locationVC)
+        let navEp = UINavigationController(rootViewController: episodesVC)
+        let navSet = UINavigationController(rootViewController: settingsVC)
+        
+        navChar.tabBarItem = UITabBarItem(title: "Characters",
+                                          image: UIImage(systemName: "person"),
+                                          tag: 1)
+        navLoc.tabBarItem = UITabBarItem(title: "Locations",
+                                          image: UIImage(systemName: "location"),
+                                          tag: 2)
+        navEp.tabBarItem = UITabBarItem(title: "Episodes",
+                                          image: UIImage(systemName: "tv"),
+                                          tag: 3)
+        navSet.tabBarItem = UITabBarItem(title: "Settings",
+                                          image: UIImage(systemName: "gear"),
+                                          tag: 4)
+        
+        for nav in [navChar, navLoc, navEp, navSet] {
+            nav.navigationBar.prefersLargeTitles = true
+        }
+        
+        setViewControllers(
+            [navChar, navLoc, navEp, navSet],
+            animated: true
+        )
+        
+    }
 
 }
 
