39cbac695d54f1bfb58981d54d2323e057aff1d3
hikmetuygur <uygurhikmet@gmail.com>
2023-01-31 03:19:15 +0300

    Added docstrings and API related files

---
+++ RickNMorty/APIClient/RMRequest.swift
@@ -0,0 +1,14 @@
+//
+//  RMRequest.swift
+//  RickNMorty
+//
+//  Created by huygur on 31.01.2023.
+//
+
+import Foundation
+
+
+/// Object that represents a singlet API call
+final class RMRequest {
+    
+}
