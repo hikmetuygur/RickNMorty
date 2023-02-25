0998e6bde4a99a6a006a22d6f07990b33edd2398
hikmetuygur <uygurhikmet@gmail.com>
2023-02-25 19:41:49 +0300

    Updated CharacterCell

---
+++ RickNMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  RMCharacterCollectionViewCellViewModel.swift
+//  RickNMorty
+//
+//  Created by huygur on 25.02.2023.
+//
+
+import Foundation
+
+struct RMCharacterCollectionViewCellViewModel {
+    public let characterName: String
+    private let characterStatus: RMCharacterStatus
+    private let characterImageUrl: URL?
+
+    //MARK: - Init
+
+    init(
+        characterName: String,
+        characterStatus: RMCharacterStatus,
+        characterImageUrl: URL?
+    ) {
+        self.characterName = characterName
+        self.characterStatus = characterStatus
+        self.characterImageUrl = characterImageUrl
+    }
+
+    public var characterStatusText: String {
+        return characterStatus.rawValue
+    }
+
+    public func fetchImage(completion: @escaping(Result <Data, Error>) -> Void) {
+        
+        // TODO: Abstract to image manager
+        guard let url = characterImageUrl else {
+            completion(.failure(URLError(.badURL)))
+            return
+        }
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { data, __, error in
+            guard let data = data, error == nil else {
+                completion(.failure(error ?? URLError(.badServerResponse)))
+                return
+            }
+            
+            completion(.success(data))
+        }
+        task.resume()
+    }
+}
